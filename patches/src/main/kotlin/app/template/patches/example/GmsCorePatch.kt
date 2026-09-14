4699e7b46dad5ea517955914243149fef06ff713
HelioFloxZ <tizenbacon@gmail.com>
2026-09-14 19:52:33 +0530

    Fix: Fixed Syntax Error in GmsCorePatch.kt

--- patches/src/main/kotlin/app/template/patches/example/GmsCorePatch.kt
+++ patches/src/main/kotlin/app/template/patches/example/GmsCorePatch.kt
@@ -317,7 +317,8 @@ val ytStudioGmsCoreSupportPatch = bytecodePatch(
         ServiceCheckFingerprint.method.addInstruction(0, "return-void")
 
         AccountValidityMonitorCheckFingerprint.method.addInstruction(
-        0,
-        "return-void",
-    )
+            0,
+            "return-void",
+        )
+    }
 }
