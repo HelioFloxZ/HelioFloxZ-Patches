1e33e3a9311b26e4f871473626c6ddf6b0b9a6ce
HelioFloxZ <tizenbacon@gmail.com>
2026-09-17 09:56:04 +0530

    Update ChangeFlagsPatch.kt

--- patches/src/main/kotlin/app/template/patches/googlephone/ChangeFlagsPatch.kt
+++ patches/src/main/kotlin/app/template/patches/googlephone/ChangeFlagsPatch.kt
@@ -5,18 +5,21 @@ import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.template.patches.shared.Constants.GOOGLE_PHONE_COMPATIBILITY
 
 @Suppress("unused")
-val callRecordingPatch = bytecodePatch(
-    name = "Call recording support",
-    description = "Enables the Google Phone call-recording eligibility check.",
+val changeFlagsPatch = bytecodePatch(
+    name = "Change flags",
+    description = "Adds customizable Google Phone feature flags.",
     default = true,
 ) {
     compatibleWith(GOOGLE_PHONE_COMPATIBILITY)
+
+    extendWith("extensions/extension.mpe")
 
     execute {
         CallRecordingCountryGateFingerprint.method.addInstructions(
             0,
             """
-                const/4 v0, 0x1
+                invoke-static {}, Lapp/template/extension/extension/ChangeFlags;->forceCallRecording()Z
+                move-result v0
                 return v0
             """.trimIndent(),
         )
